28a8b81cb0102b94054a2e0beec2beae04efd49c
arudkov14 <andrey14@ru.is>
2018-10-11 12:12:29 +0000

    Third test added

--- src/calculator.test.js
+++ src/calculator.test.js
@@ -2,9 +2,13 @@ const add = require('./calculator');
 
 test('Should be 1 if input is 1', () => {
 
-    expect(add("1")).toBe(1);
+    expect(add('1')).toBe(1);
     });
 
-Test('Should be 0 if no imput', () =>{
-    expect(add("0")).toBE(0)
+test('Should be 0 if no input', () =>{
+    expect(add('0')).toBE(0)
+});
+
+test('Should return expected sum ',() => {
+    expect(add('2,3,7')).toBe(12)
 });
