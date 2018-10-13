e90c705f53f3f88cd995a2a9a59d6da545003cb8
arudkov14 <andrey14@ru.is>
2018-10-13 11:25:10 +0000

    New Test added

--- src/calculator.test.js
+++ src/calculator.test.js
@@ -4,13 +4,21 @@ test('Should return expected sum ',() => {
     expect(add('2, 3, 7')).toBe(12);
 });
 
-test('Should be 1 if input is 1', () => {
+test('Should return expected sum with more numbers and larger numb ',() => {
+    expect(add('2, 3, 7, 2, 4 ,5 ,78,777')).toBe(878);
+});
 
+test('Should be 1 if input is 1', () => {
     expect(add('1')).toBe(1);
     });
 
 test('Should be 0 if no input', () =>{
     expect(add('0')).toBe(0);
 });
+
+test('Allow new line \n separators', () => {
+    expect(add('1,2 \n3')).toBe(6);
+});
+
 
 
