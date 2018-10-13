e92ac770d848f3b8ea84fc5cf705786af7fc41ef
arudkov14 <andrey14@ru.is>
2018-10-13 11:46:39 +0000

    New line success

--- src/calculator.js
+++ src/calculator.js
@@ -4,14 +4,19 @@
 
 function add(String)
 {
-    const separator = ',';
+    const separators = [',','\\n'];
     
     if(String.length == 0)
     {
         return 0;
     }
 
-    const list = String.split(separator)
+    if(String.match(/^\/\/(D+)\n/))
+    {
+        separators.push(String.match(/^\/\/(\D+)\n/)[1]);
+    }
+    const list = String.split(new RegExp(`[${separators.join('')}]`))
+    .filter(item => !isNaN(parseInt(item)));
 
     let sum = 0;
     for(let i=0; i<list.length; i++)
